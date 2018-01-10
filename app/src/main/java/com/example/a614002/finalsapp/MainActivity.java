60679ddc229541e4dff2b3a7610ff12b034869fd
614002 <joshua.spencer010203@gmail.com>
2018-01-10 10:02:33 -0600

    Initial commit

---
+++ app/src/main/java/com/example/a614002/finalsapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a614002.finalsapp;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MediaPlayer welcome = MediaPlayer.create(getApplicationContext(), R.raw.AudioFile1);
+        welcome.start();
+    }
+}
